b8f35bc82e5f7f285765aa55231f09d912bd3723
夏先生 <1084581212@qq.com>
2024-06-21 11:08:17 +0800

    delete config_path function

--- src/helper.php
+++ src/helper.php
@@ -7,8 +7,8 @@ if (! function_exists('config_path')) {
      * @param  string  $path
      * @return string
      */
-    function config_path($path = '')
+   /* function config_path($path = '')
     {
         return app()->basePath().'/config'.($path ? '/'.$path : $path);
-    }
+    }*/
 }
